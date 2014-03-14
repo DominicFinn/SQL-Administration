462c61214e9003b56ed95c1c56fffe26da59afcf
DominicFinn <dom.finn@gmail.com>
2014-03-14 16:40:23 +0000

    Continued investigation into execution plans

--- QueryOptimisation/QueryOptimisation-UnderstandingQueryPlans.sql
+++ QueryOptimisation/QueryOptimisation-UnderstandingQueryPlans.sql
@@ -9,7 +9,7 @@
 select * from sys.dm_exec_query_optimizer_info
 where counter in (
 	'optimizations',
-	'trivia plan',
+	'trivial plan',
 	'search 0',
 	'search 1',
 	'search 2'	
@@ -40,4 +40,40 @@ time to find an execution plan that works best and one that is acceptably fast.
 7. if the plan is < 1 use it else try 
 
 8. explore other ideas.... (need to research some more about this...) 
+
+-- basically, the more queries you have moving up through the searches, the more problems you have
+
+-- Looking at the exectuion plan... 
+
+Join / Loops
+
+3 types of joins
+
+** Nested Join
+_Good for small tables, make sure tgere is an index on the inner table on the join key_
+
+Scans all the rows in the outer table and then scans the inner table. If the row on the inner matches the row on the outer
+then it's included. 
+
+This tends to work well if the inner table has few rows and you are joining to a larger table, a good example would be doing
+from smalltable s left outer join largertable l on s.indexedkey = l.key
+
+This join types runs terribly when the tables get large
+
+** Merge
+_Good for medium sized tables, make sure there are indexes on the join keys_
+
+needs its tables (inputs) to be sorted, runs down both tables at the same time.... (need to know more about this...)
+
+** Hash 
+_Good for medium to large tables_
+
+Works out which table is smallest and puts it in a has table, then runs over the large table matching keys in the hash.
+This method scales and works well in parralel environments apparently... 
+
+* Looking at Query plans	
+
+When you look at the query plans, if you look at a 
+
 */
+
